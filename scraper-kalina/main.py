0192c976f23dcf5fc03230f446d723395fc82742
Kalina Karneva <76961675+KalinaKar@users.noreply.github.com>
2021-06-10 19:20:40 +0300

    Create main.py

---
+++ scraper-kalina/main.py
@@ -0,0 +1,18 @@
+'''
+Created on Jun 7, 2021
+
+@author: kalina
+'''
+
+import scraper.py
+
+if __name__ == '__main__':
+    import time
+    twenty_urls = UrlScraper.get_twenty_posts()
+    post_scraper = PostScraper(twenty_urls)
+    posts = post_scraper.get_last_posts()
+    
+    filename = str(time.time()) +  "posts.txt"
+
+    for p in posts:
+        p.print_to_file(filename)
