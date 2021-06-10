d511f4dd3b8f950a0814010bba0544821e57c1c0
Kalina Karneva <76961675+KalinaKar@users.noreply.github.com>
2021-06-10 19:19:19 +0300

    Update scraper.py

--- scraper-kalina/scraper.py
+++ scraper-kalina/scraper.py
@@ -74,15 +74,3 @@ class PostScraper():
         text = post.find('div', class_='entry-content').text
 
         return Post(title, date, text)
-
-
-if __name__ == '__main__':
-    import time
-    twenty_urls = UrlScraper.get_twenty_posts()
-    post_scraper = PostScraper(twenty_urls)
-    posts = post_scraper.get_last_posts()
-    
-    filename = str(time.time()) +  "posts.txt"
-
-    for p in posts:
-        p.print_to_file(filename)
