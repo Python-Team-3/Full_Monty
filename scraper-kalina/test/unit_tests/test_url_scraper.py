9853bdf44c2f402f56577e5738e76ffe9ae4960c
Kalina Karneva <76961675+KalinaKar@users.noreply.github.com>
2021-06-11 01:44:44 +0300

    Update test_url_scraper.py

--- scraper-kalina/test/unit_tests/test_url_scraper.py
+++ scraper-kalina/test/unit_tests/test_url_scraper.py
@@ -1,7 +1,7 @@
-from os import link
 from module.url_scraper import UrlScraper
 
 def test_url_scraper():
     links = UrlScraper.get_twenty_posts()
     assert len(links) == 20
-    assert links[0].startswith("https")
+    for link in links:
+        assert link.startswith("https")
