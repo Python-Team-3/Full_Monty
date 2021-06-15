a33c3d34e5ab0cf595dd43b9e405f81b07d4ea15
Siyana Hristova <siyah.hristova@gmail.com>
2021-06-15 22:06:49 +0300

    Delete json.py

--- test/module/json.py
+++
@@ -1,8 +0,0 @@
-import json
-from urllib.request import urlopen
-
-with urlopen('https://www.kulinarno-joana.com/') as response:
-    source = response.read()
-
-data = json.loads(source)
-print(json.dumps(data, indent=2))
