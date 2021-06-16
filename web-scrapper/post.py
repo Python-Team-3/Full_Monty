f12b7d699e60821d2bf192a5e6a349dd69c69f88
Siyana Hristova <siyah.hristova@gmail.com>
2021-06-16 17:34:15 +0300

    Add files via upload
    test JSON

---
+++ web-scrapper/post.py
@@ -0,0 +1,19 @@
+import json
+
+class Post():
+    
+    def __init__(self, title, date, text) -> None:
+        self.title = title
+        self.date = date
+        self.text = text
+    
+    def print_to_file(self, filename) -> None:
+        with open(filename, 'a+') as f:
+            jsonTitle = json.dumps(self.title)
+            jsonDate = json.dumps(self.title)
+            jsonText = json.dumps(self.title)
+
+            json.write(jsonTitle)
+            json.write(jsonDate)
+            json.write(jsonText)
+            json.write((20 * '-') + '\n')
