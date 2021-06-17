5e77778b86f1a906a23fe021e9ccd62441619793
Ventislav <clappingmonkey33@gmail.com>
2021-06-17 09:23:44 +0300

    add mock json file, almost finish the index file and add two more templates

--- frontend/app/routes.py
+++ frontend/app/routes.py
@@ -1,21 +1,124 @@
 from app import app
 from flask import render_template
+import json
 
 @app.route('/')
 @app.route('/index')
+
+
 
 def index():
 #    return "I did not hit her, it's not true, it's bullshit, I did not... Oh, hi Mark!"
 
     user = {'username': 'Pesho'}
-    posts = [
-            {
-                'author': {'username': 'Georgi'},
-                'body': 'Just testing here!'
-            },
-            {
-                'author': {'username': 'Susan'},
-                'body': 'The second test is way more beautiful!!'
-            }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+
+    f = open('app/data.json', 'r')
+
+    articles = json.load(f)['articles']
+
+    f.close()
+ 
+
+    # articles = [
+    #             {
+    #                 'post': {'title': 'Test title'},
+    #                 'time': {'postedOn' : 'преди 3 дни'},
+    #                 'link': {'postURL': '_blank'}
+    #             },
+    #             {
+    #                 'post': {'title': 'Test title'},
+    #                 'time': {'postedOn' : 'преди 3 дни'},
+    #                 'link': {'postURL': '_blank'}
+    #             },
+    #             {
+    #                 'post': {'title': 'Test title'},
+    #                 'time': {'postedOn' : 'преди 3 дни'},
+    #                 'link': {'postURL': '_blank'}
+    #             },
+    #             {
+    #                 'post': {'title': 'Test title'},
+    #                 'time': {'postedOn' : 'преди 3 дни'},
+    #                 'link': {'postURL': '_blank'}
+    #             },
+    #             {
+    #                 'post': {'title': 'Test title'},
+    #                 'time': {'postedOn' : 'преди 3 дни'},
+    #                 'link': {'postURL': '_blank'}
+    #             },
+    #             {
+    #                 'post': {'title': 'Test title'},
+    #                 'time': {'postedOn' : 'преди 3 дни'},
+    #                 'link': {'postURL': '_blank'}
+    #             },
+    #             {
+    #                 'post': {'title': 'Test title'},
+    #                 'time': {'postedOn' : 'преди 3 дни'},
+    #                 'link': {'postURL': '_blank'}
+    #             },
+    #             {
+    #                 'post': {'title': 'Test title'},
+    #                 'time': {'postedOn' : 'преди 3 дни'},
+    #                 'link': {'postURL': '_blank'}
+    #             },
+    #             {
+    #                 'post': {'title': 'Test title'},
+    #                 'time': {'postedOn' : 'преди 3 дни'},
+    #                 'link': {'postURL': '_blank'}
+    #             },
+    #                             {
+    #                 'post': {'title': 'Test title'},
+    #                 'time': {'postedOn' : 'преди 3 дни'},
+    #                 'link': {'postURL': '_blank'}
+    #             },
+    #             {
+    #                 'post': {'title': 'Test title'},
+    #                 'time': {'postedOn' : 'преди 3 дни'},
+    #                 'link': {'postURL': '_blank'}
+    #             },
+    #             {
+    #                 'post': {'title': 'Test title'},
+    #                 'time': {'postedOn' : 'преди 3 дни'},
+    #                 'link': {'postURL': '_blank'}
+    #             },
+    #             {
+    #                 'post': {'title': 'Test title'},
+    #                 'time': {'postedOn' : 'преди 3 дни'},
+    #                 'link': {'postURL': '_blank'}
+    #             },
+    #             {
+    #                 'post': {'title': 'Test title'},
+    #                 'time': {'postedOn' : 'преди 3 дни'},
+    #                 'link': {'postURL': '_blank'}
+    #             },
+    #             {
+    #                 'post': {'title': 'Test title'},
+    #                 'time': {'postedOn' : 'преди 3 дни'},
+    #                 'link': {'postURL': '_blank'}
+    #             },
+    #             {
+    #                 'post': {'title': 'Test title'},
+    #                 'time': {'postedOn' : 'преди 3 дни'},
+    #                 'link': {'postURL': '_blank'}
+    #             },
+    #             {
+    #                 'post': {'title': 'Test title'},
+    #                 'time': {'postedOn' : 'преди 3 дни'},
+    #                 'link': {'postURL': '_blank'}
+    #             },
+    #             {
+    #                 'post': {'title': 'Test title'},
+    #                 'time': {'postedOn' : 'преди 3 дни'},
+    #                 'link': {'postURL': '_blank'}
+    #             },
+    #             {
+    #                 'post': {'title': 'Test title'},
+    #                 'time': {'postedOn' : 'преди 3 дни'},
+    #                 'link': {'postURL': '_blank'}
+    #             },
+    #             {
+    #                 'post': {'title': 'Test title'},
+    #                 'time': {'postedOn' : 'преди 3 дни'},
+    #                 'link': {'postURL': '_blank'}
+    #             }
+    #             ]
+    return render_template('index.html', title='Home', user=user, articles=articles)
