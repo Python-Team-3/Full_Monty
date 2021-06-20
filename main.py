d2129f6a4828be0f23bf03d54360131435e5f4cf
Ventislav <clappingmonkey33@gmail.com>
2021-06-20 18:22:13 +0300

    fix the problem of the merge earlier

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from module.url_scraper import UrlScraper
 from module.post_scraper import PostScraper
-import module.post
-import json
+from module.post import Post
+import time
 
 if __name__ == '__main__':
-
+    
     twenty_urls = UrlScraper.get_twenty_posts()
     post_scraper = PostScraper(twenty_urls)
-    filename = str(time.time()) +  "posts.json"
-    posts = post_scraper.get_last_posts()
-    for p in posts:
-        p.print_to_file(filename)
+    filename = str(time.time()) +  "posts"
+
+    scraped_posts = []
+
+    for p in post_scraper.get_last_posts():
+        scraped_posts.append(p.get_as_json_object())
+    
+    Post.print_to_json_file(scraped_posts, filename)
--- module/post_scraper.py
+++ module/post_scraper.py
@@ -1,7 +1,5 @@
 import requests 
 from bs4 import BeautifulSoup
-from collections import Counter
-import re
 
 from module.post import Post
 
@@ -10,17 +8,14 @@ class PostScraper():
     def __init__(self, post_urls):
         self.post_urls = post_urls
 
-
-    # gets the posts from blog and append them to list
     def get_last_posts(self) -> list:
         posts = []
-        i = 0
+
         for url in self.post_urls:
             posts.append(self._read_post(url))
-
+        
         return posts
-
-    # read posts from blog
+    
     def _read_post(self, url_post) -> Post:
         r = requests.get(url_post)
 
@@ -33,19 +28,10 @@ class PostScraper():
         paragraphs = self._get_paragraphs(body)
 
         comments_soup = soup.find('ol', class_='comment-list')
-
+        
         comments = None if comments_soup is None else self._get_comments(comments_soup) 
 
         text = self._remove_linked_posts(body)
-        common = self._get_most_common_words(text.split())
-
-        return Post(title, date, text, comments, common)
-
-    def _get_most_common_words(self, word_list):
-
-        words_to_count = (word for word in word_list if len(word)>3)
-        c = Counter(words_to_count)
-        return c.most_common(3)
 
         return Post(title, date, text, comments=comments, first_paragraphs=paragraphs)
 
@@ -79,7 +65,6 @@ class PostScraper():
             child.decompose()
         return comment_author.text.strip()
 
-    # remove linked posts
     def _remove_linked_posts(self, body) -> str:
 
         selects = body.find_all("ul", {"id": "klnArticleRelatedPosts"})
@@ -91,16 +76,3 @@ class PostScraper():
             text += tag.text + '\n'
 
         return text
-
-    def _get_comments(self, body) -> str: 
-        selects = body.find_all("li", id = re.compile('^comment-'))
-
-        data = {}
-        data['comments'] = []
-
-        for tag in selects:
-            data['comments'].append({
-            tag.find("footer", class_='comment-meta').text : tag.find("div", class_='comment-content').text
-
-            })
-        return data
