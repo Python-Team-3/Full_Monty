145baa84ef762297b06819a5b15cd0e5a6e17c06
Kalina Karneva <76961675+KalinaKar@users.noreply.github.com>
2021-06-21 10:37:00 +0300

    Added Docstrings

--- module/post_scraper.py
+++ module/post_scraper.py
@@ -1,4 +1,4 @@
-import requests
+import requests 
 from bs4 import BeautifulSoup
 
 from module.post import Post
@@ -6,17 +6,38 @@ from module.post import Post
 class PostScraper():
 
     def __init__(self, post_urls):
+        """
+        A class responsible for scraping each post
+
+        Args:
+            post_urls (list): A list of certain post urls, which are going to be scraped
+        """
         self.post_urls = post_urls
 
     def get_last_posts(self) -> list:
+        """
+        Calls the scraping of each individual post in the url list
+
+        Returns:
+            list: A list of Post objects
+        """
         posts = []
 
         for url in self.post_urls:
             posts.append(self._read_post(url))
-
+        
         return posts
-
+    
     def _read_post(self, url_post) -> Post:
+        """
+        Reads certain content of the post and returns it divided into arguments of an instance of class Post
+
+        Args:
+            url_post (str): A string of the post's url
+
+        Returns:
+            Post: Instance of class Post
+        """
         r = requests.get(url_post)
 
         soup = BeautifulSoup(r.text, features='html.parser')
@@ -28,14 +49,23 @@ class PostScraper():
         paragraphs = self._get_paragraphs(body)
 
         comments_soup = soup.find('ol', class_='comment-list')
-
-        comments = None if comments_soup is None else self._get_comments(comments_soup)
+        
+        comments = None if comments_soup is None else self._get_comments(comments_soup) 
 
         text = self._remove_linked_posts(body)
 
         return Post(title, date, text, comments=comments, first_paragraphs=paragraphs)
 
     def _get_paragraphs(self, body):
+        """
+        Filters only the text from all html subtags of the post by iterating through each one of them
+
+        Args:
+            body (bs4.element.Tag): The whole body of the article
+
+        Returns:
+            list: A list of all paragraphs in the article
+        """
         paragraphs = []
 
         for paragraph in body.find_all('p'):
@@ -47,10 +77,25 @@ class PostScraper():
         return paragraphs
 
     def _remove_images(self, paragraph):
+        """
+        Decomposes the html paragraph tag if it contains an image
+
+        Args:
+            paragraph (bs4.element.Tag): Tag holding a paragraph 
+        """
         for photo in paragraph.findChildren('img'):
             photo.decompose()
 
-    def _get_comments(self, comments_soup):
+    def _get_comments(self, comments_soup) -> list:
+        """
+        Filters out the comment authors and comment contents and returns them as a list of tuples
+
+        Args:
+            comments_soup (bs4.element.Tag): Tag holding all the comments in the post
+
+        Returns:
+            list: List of tuples - (author, comment)
+        """
 
         comments_list = []
         for li in comments_soup.find_all('li'):
@@ -61,11 +106,29 @@ class PostScraper():
         return comments_list
 
     def _remove_children(self, comment_author) -> str:
+        """
+        Removes all the subtags of a given element
+
+        Args:
+            comment_author (bs4.element.Tag): Tag holding the comment author info
+
+        Returns:
+            str: Name of comment author
+        """
         for child in comment_author.findChildren():
             child.decompose()
         return comment_author.text.strip()
 
     def _remove_linked_posts(self, body) -> str:
+        """
+        Removes linked urls from the article body
+
+        Args:
+            body (bs4.element.Tag): The whole body of the article
+
+        Returns:
+            str: Article content with paragraphs divided by new lines
+        """
 
         selects = body.find_all("ul", {"id": "klnArticleRelatedPosts"})
         for match in selects:
--- module/url_scraper.py
+++ module/url_scraper.py
@@ -1,4 +1,4 @@
-import requests
+import requests 
 from bs4 import BeautifulSoup
 
 class UrlScraper():
@@ -9,6 +9,15 @@ class UrlScraper():
         pass
 
     def _get_post_urls(page_url) -> list:
+        """
+        Scrapes post urls from the index pages and stores them in a list
+
+        Args:
+            page_url (str): The url of the blog we are going to scrape
+
+        Returns:
+            list: List of post urls
+        """
         r = requests.get(page_url)
         soup = BeautifulSoup(r.text, features='html.parser')
         articles = soup.find_all("article")
@@ -19,9 +28,15 @@ class UrlScraper():
         return url_list
 
     def get_twenty_posts() -> list:
+        """
+        Iterates through the pages until certain number of post url is scraped
+
+        Returns:
+            list: Sliced list of post urls
+        """
         urls = []
         page_number = 1
-        while len(urls) < UrlScraper.POSTS_TO_SCRAPE:
+        while len(urls) < UrlScraper.POSTS_TO_SCRAPE: 
             page_url = UrlScraper.BLOG_URL + f"page/{page_number}/"
             urls.extend(UrlScraper._get_post_urls(page_url))
             page_number += 1
